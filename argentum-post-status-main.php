b8be3b25ad13886b2bc92f302fd6be5789e5b4ff
Karthik Narasimhan <karthik@silverscreen.in>
2020-03-18 19:47:12 +0000

    tagging

--- argentum-post-status-main.php
+++ argentum-post-status-main.php
@@ -5,7 +5,7 @@ Main plugin file
 */
 
 namespace ArgentumPostStatus;
-define('ARGENTUM_POST_STATUS_VERSION','01.00.00');
+define('ARGENTUM_POST_STATUS_VERSION','01.00.01');
 define('ARGENTUM_POST_STATUS_URL' , plugins_url( '/', __FILE__ ) );
 
 class ArgentumPostStatus
